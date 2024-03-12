049eaf1cebfb4211c9af45c07be2d52d2573ae04
DEEPAK JAISWAL <jaiswalpak450@gmail.com>
2024-03-12 20:29:12 +0545

    done with total price

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -6,42 +6,17 @@ import chicken from "./Assets/items/chicken.webp";
 import mutton from "./Assets/items/mutton.jpeg";
 import vegBiryani from "./Assets/items/vegBiryani.jpeg";
 import Button from "@mui/material/Button";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 const Menu = () => {
-  // State to store the quantity of each item
   const [itemQuantities, setItemQuantities] = useState({});
   const [items, setItems] = useState({});
-
-  // Function to handle increasing quantity
-  const increaseQuantity = (itemId, item) => {
-    const newQuantities = { ...itemQuantities };
-    const newItems = { ...items };
-    if (newItems.hasOwnProperty(itemId)) {
-      newItems[itemId].count = (newItems[itemId].count || 0) + 1;
-    } else {
-      newItems[itemId] = { ...item, count: 1 };
-    }
-    newQuantities[itemId] = (newQuantities[itemId] || 0) + 1;
-    calculateTotalCount(newQuantities);
-    setItemQuantities(newQuantities);
-    setItems(newItems);
-  };
-  console.log(items);
-  // Function to handle decreasing quantity
-  const decreaseQuantity = (itemId, item) => {
-    const newQuantities = { ...itemQuantities };
-    if (newQuantities[itemId] && newQuantities[itemId] > 0) {
-      newQuantities[itemId] -= 1;
-      calculateTotalCount(newQuantities);
-      setItemQuantities(newQuantities);
-    }
-  };
-
+  const [totalCount, setTotalCount] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
   const menuItems = [
     {
       id: 1,
       itemName: "Fish Biryani",
-      price: "140",
+      price: 140,
       src: biryani,
       category: "nonVeg",
       count: 0,
@@ -49,7 +24,7 @@ const Menu = () => {
     {
       id: 2,
       itemName: "Chicken Biryani",
-      price: "130",
+      price: 130,
       src: chicken,
       category: "nonVeg",
       count: 0,
@@ -57,7 +32,7 @@ const Menu = () => {
     {
       id: 3,
       itemName: "Mutton Biryani",
-      price: "150",
+      price: 150,
       src: mutton,
       category: "nonVeg",
       count: 0,
@@ -65,7 +40,7 @@ const Menu = () => {
     {
       id: 4,
       itemName: "Veg Biryani",
-      price: "110",
+      price: 110,
       src: vegBiryani,
       category: "veg",
       count: 0,
@@ -73,7 +48,7 @@ const Menu = () => {
     {
       id: 5,
       itemName: "Biryani",
-      price: "120",
+      price: 120,
       src: fishBiryani,
       category: "veg",
       count: 0,
@@ -81,7 +56,7 @@ const Menu = () => {
     {
       id: 6,
       itemName: "Veg Biryani",
-      price: "110",
+      price: 110,
       src: vegBiryani,
       category: "veg",
       count: 0,
@@ -89,20 +64,54 @@ const Menu = () => {
     {
       id: 7,
       itemName: "Biryani",
-      price: "120",
+      price: 120,
       src: fishBiryani,
       category: "veg",
       count: 0,
     },
   ];
-  const [totalCount, setTotalCount] = useState(0);
+
+  const increaseQuantity = (itemId, item) => {
+    const newQuantities = { ...itemQuantities };
+    const newItems = { ...items };
+    // eslint-disable-next-line no-prototype-builtins
+    if (newItems.hasOwnProperty(itemId)) {
+      newItems[itemId].count = (newItems[itemId].count || 0) + 1;
+    } else {
+      newItems[itemId] = { ...item, count: 1 };
+    }
+    newQuantities[itemId] = (newQuantities[itemId] || 0) + 1;
+    calculateTotalCount(newItems);
+    setItemQuantities(newQuantities);
+    setItems(newItems);
+  };
+
+  const decreaseQuantity = (itemId, item) => {
+    const newQuantities = { ...itemQuantities };
+    const newItems = { ...items };
+    if (newQuantities[itemId] && newQuantities[itemId] > 0) {
+      newQuantities[itemId] -= 1;
+      newItems[itemId].count = (newItems[itemId].count || 0) - 1;
+      if (newQuantities[itemId] == 0) {
+        delete newItems[itemId];
+      }
+      calculateTotalCount(newItems);
+      setItemQuantities(newQuantities);
+      setItems(newItems);
+    }
+  };
+
   const calculateTotalCount = (items) => {
     let sum = 0;
+    let total = 0;
     Object.keys(items).forEach((item, index) => {
-      sum = sum + items[item];
+      sum = sum + items[item].count;
+      total = total + items[item].count * items[item].price;
     });
     setTotalCount(sum);
+    setTotalPrice(total);
   };
+
   return (
     <div className={styles.menuWrapper}>
       <h1>Menu</h1>
@@ -142,8 +151,15 @@ const Menu = () => {
           <span>Total</span> items <span>bought</span> {totalCount}
         </div>
         <div className={styles.right}>
-          <span>Total price</span> &#8377; XXX
-          <Button variant="outlined">Order Now</Button>
+          <span>Total price</span> &#8377; {totalPrice}
+          <Button
+            variant="outlined"
+            onClick={() => {
+              alert("You have ordered successfully!");
+            }}
+          >
+            Order Now
+          </Button>
         </div>
       </div>
     </div>
